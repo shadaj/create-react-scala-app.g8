0b835b05aad9c1f968313fa4c6bc67d864fa440d
Jason Pickens <jasonpickensnz@gmail.com>
2020-05-19 05:33:26 +1200

    Use lowercase filenames for webpack (#13)

--- src/main/g8/webpack/scalajs-entry.js
+++ src/main/g8/webpack/scalajs-entry.js
@@ -1,13 +1,13 @@
 if (process.env.NODE_ENV === "production") {
-    const opt = require("./$name$-opt.js");
+    const opt = require("./$name;format="lower"$-opt.js");
     opt.main();
     module.exports = opt;
 } else {
     var exports = window;
-    exports.require = require("./$name$-fastopt-entrypoint.js").require;
+    exports.require = require("./$name;format="lower"$-fastopt-entrypoint.js").require;
     window.global = window;
 
-    const fastOpt = require("./$name$-fastopt.js");
+    const fastOpt = require("./$name;format="lower"$-fastopt.js");
     fastOpt.main()
     module.exports = fastOpt;
 
