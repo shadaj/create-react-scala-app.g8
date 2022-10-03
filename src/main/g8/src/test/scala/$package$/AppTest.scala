049fa0b2d4de7c3c3c83e723626c495308a3fa4c
Philip R. Kensche <pkennsche@mailbox.org>
2022-10-03 23:05:30 +0200

    Fix AppTest.scala.

--- src/main/g8/src/test/scala/$package$/AppTest.scala
+++ src/main/g8/src/test/scala/$package$/AppTest.scala
@@ -3,9 +3,9 @@ package $package$
 import slinky.web.ReactDOM
 import org.scalajs.dom.document
 
-import org.scalatest.FunSuite
+import org.scalatest.funsuite.AnyFunSuite
 
-class AppTest extends FunSuite {
+class AppTest extends AnyFunSuite {
   test("Renders without crashing") {
     val div = document.createElement("div")
     ReactDOM.render(App(), div)
